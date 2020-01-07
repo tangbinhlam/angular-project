f9c1115f2a87881917dd866f3dc3a580e5088677
osalam <tangbinhlam@gmail.com>
2020-01-07 16:05:16 -0600

    [Integrate with Material] - add simple router

---
+++ src/app/modules/overview/overview/overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'osalam-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.less']
+})
+export class OverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
