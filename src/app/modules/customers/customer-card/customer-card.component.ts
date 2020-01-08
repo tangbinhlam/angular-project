f334a0267748fa1006d7912ec482dc394671e43b
osalam <tangbinhlam@gmail.com>
2020-01-07 22:30:55 -0600

    [Integrate with Material] - apply Cards

---
+++ src/app/modules/customers/customer-card/customer-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'osalam-customer-card',
+  templateUrl: './customer-card.component.html',
+  styleUrls: ['./customer-card.component.less']
+})
+export class CustomerCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/customers/customers.module.ts
+++ src/app/modules/customers/customers.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
+import {MaterialModule} from '../../shared/material-module';
 
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent, CustomerCardComponent],
   imports: [
     CommonModule,
-    CustomersRoutingModule
+    CustomersRoutingModule,
+    MaterialModule,
   ]
 })
 export class CustomersModule { }
