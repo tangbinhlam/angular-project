51cf1955c7bf15fa22a2b69a78aade6e14d1bbf0
osalam <tangbinhlam@gmail.com>
2020-01-07 21:56:24 -0600

    [Integrate with Material] - apply Tabs

--- src/app/modules/orders/orders.module.ts
+++ src/app/modules/orders/orders.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { OrdersRoutingModule } from './orders-routing.module';
-import { OrderListComponent } from './order-list/order-list.component';
+import {OrdersRoutingModule} from './orders-routing.module';
+import {OrderListComponent} from './order-list/order-list.component';
+import {MaterialModule} from '../../shared/material-module';
 
 
 @NgModule({
   declarations: [OrderListComponent],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    MaterialModule,
   ]
 })
-export class OrdersModule { }
+export class OrdersModule {
+}
