8a61ef335adac6d27494216e2dc1838c7c19376c
osalam <tangbinhlam@gmail.com>
2020-01-09 22:36:18 -0600

    [Integrate with Jest] - move config jest to file

---
+++ jest.base.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: [
+    './setupJest.ts'
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/src/test.ts'
+  ],
+  globals: {
+    "ts-jest": {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: "\\.html$",
+      astTransformers: [
+        '<rootDir>/node_modules/jest-preset-angular/InlineHtmlStripStylesTransformer'
+      ]
+    }
+  }
+};
